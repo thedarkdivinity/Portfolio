09dbf39506310befd4bdf14761c7ea11cf970bec
thedarkdivinity <sayush.kamat@rediffmail.com>
2021-04-12 19:54:34 +0530

    fixed pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Sayush Kamat`,
+        short_name: `Sayush Kamat`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
