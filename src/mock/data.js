7aa5ffef116e3e2c723f545035e5cc9a835811af
thedarkdivinity <sayush.kamat@rediffmail.com>
2021-04-18 00:20:25 +0530

    optimized the images and added the oride education website

--- src/mock/data.js
+++ src/mock/data.js
@@ -27,16 +27,16 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project1.png',
-    title: 'Gift Junction Website',
-    info: 'This is a website made for a local gift shop using html5,css3,Bootstrap 4  javascript and JQuery ',
-    info2: 'Animate on Scroll Library is used for smooth 60fps scroll animations',
-    url: 'https://giftjunction.netlify.app',
+    img: 'prideca.webp',
+    title: 'Pride Commerce Education Website',
+    info: 'This is a  fullstack MERN website made for an online commerce course selling business using the MERN stack ',
+    info2: 'This website was designed and developed in a team of 9 developers as a part of DSC VJTI.I worked on the react frontend part along with 4 other team members as well as I was the link between the DSC Design Team and the Frontend Team.The website uses two factor OTP based Auth System.My contributions include Landing page,navbar,Animate on Scroll Setup,Axios set up,Marketplace , Product Details page ,OTP Verification page as well as Connecting to the backend APIs ',
+    url: 'https://prideca.in',
     repo: 'https://github.com/thedarkdivinity/giftjunction.git', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project2.png',
+    img: 'project2.webp',
     title: 'DSC VJTI Website',
     info: 'As a member of the Web Team of Developer Student Club VJTI,developed this website in a team of 5 members.My major contribution was in hosting the Website,setting up the domain,setting up the netlify form submission and the page animations.',
     info2: 'Technologies used are Bootstrap 4,CSS3,HTML5 and Javascript .Animate On Scroll Library was used for Page Animations',
@@ -45,7 +45,17 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: 'project3.png',
+    img: 'project1.webp',
+    title: 'Gift Junction Website',
+    info: 'This is a website made for a local gift shop using html5,css3,Bootstrap 4  javascript and JQuery ',
+    info2: 'Animate on Scroll Library is used for smooth 60fps scroll animations',
+    url: 'https://giftjunction.netlify.app',
+    repo: 'https://github.com/thedarkdivinity/giftjunction.git', // if no repo, the button will not show up
+  },
+  
+  {
+    id: nanoid(),
+    img: 'project3.webp',
     title: 'FLY HIGH NGO',
     info: 'A React Landing Page for an upcoming NGO .I volunteered to develop a website for this NGO ',
   
